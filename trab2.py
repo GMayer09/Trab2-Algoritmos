b0dcea3d981349a2b1546d494dd2bb255d170c6e
GMayer09 <guilhermecd2005@gmail.com>
2025-07-04 15:41:23 -0300

    class commit

--- trab2.py
+++ trab2.py
@@ -1,2 +1,31 @@
 from enum import Enum, auto
 from dataclasses import dataclass
+
+class CriterioDesempate(Enum):
+    '''
+    Essa classe vai desempenhar em relação ao critério de desempate dos jogos do Brasileirão.
+    '''
+    PONTO = auto()
+    VITORIA = auto()
+    SALDO_GOL = auto()
+    LETRA = auto()
+
+
+@dataclass
+class Jogo:
+    '''
+    Essa classe vai desempenhar em relação ao jogo do Brasileirão, tendo como atributos: anfitrião(Mandante),
+    gols do anfitrião, visitante e gols do visitante.
+    '''
+    anfitriao: str
+    anf_gol: int
+    visitante: str
+    vis_gol: int
+
+
+def classificacao(jogo: Jogo, criterio: CriterioDesempate) -> int:
+    '''
+    A função vai receber um jogo *jogo: Jogo* e criterio *criterio: CriterioDesempate*,
+    vai calcular a tabela dos times em relação aos jogos e vai retornar um inteiro.
+    Exemplos:
+    '''
